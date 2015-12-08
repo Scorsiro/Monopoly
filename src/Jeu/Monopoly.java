2454cb9a09169aa936f765820378948196c07ef7
RaboliotTheGreat <raboliotthegreat@mailoo.org>
2015-12-08 15:58:05 +0100

    Initial commit

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -1,7 +1,11 @@
 package Jeu;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Monopoly {
@@ -9,7 +13,12 @@ public class Monopoly {
 	private final int _nbHotels = 12;
         private ArrayList<Joueur> _joueurs;
         private HashMap<Integer, Carreau> _Carreaux;
-
+        private IHM _IHM;
+        
+        public Monopoly(String dataFilename){
+		buildGamePlateau(dataFilename);
+	}
+        
 	public void jouerUnCoup(Joueur j) {
 		
 	}
@@ -19,7 +28,7 @@ public class Monopoly {
 	}
 
 	public int calculArRest() {
-		//TAMERE
+	
 	}
 
 	public int genererChiffreDés() {
@@ -33,4 +42,59 @@ public class Monopoly {
 	public void mettreEnPrison(Joueur j) {
 		
 	}
+        
+        private void buildGamePlateau(String dataFilename)
+	{
+		try{
+			ArrayList<String[]> data = readDataFile(dataFilename, ",");
+			
+			//TODO: create cases instead of displaying
+			for(int i=0; i<data.size(); ++i){
+				String caseType = data.get(i)[0];
+				if(caseType.compareTo("P") == 0){
+					System.out.println("Propriété :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("G") == 0){
+					System.out.println("Gare :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("C") == 0){
+					System.out.println("Compagnie :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("CT") == 0){
+					System.out.println("Case Tirage :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("CA") == 0){
+					System.out.println("Case Argent :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("CM") == 0){
+					System.out.println("Case Mouvement :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else
+					System.err.println("[buildGamePleateau()] : Invalid Data type");
+			}
+			
+		} 
+		catch(FileNotFoundException e){
+			System.err.println("[buildGamePlateau()] : File is not found!");
+		}
+		catch(IOException e){
+			System.err.println("[buildGamePlateau()] : Error while reading file!");
+		}
+	}
+        private ArrayList<String[]> readDataFile(String filename, String token) throws FileNotFoundException, IOException
+	{
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		
+		BufferedReader reader  = new BufferedReader(new FileReader(filename));
+		String line = null;
+		while((line = reader.readLine()) != null){
+			data.add(line.split(token));
+		}
+		reader.close();
+		
+		return data;
+	}
+        
+        
+        
 }
