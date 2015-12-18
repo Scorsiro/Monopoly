b20e9b76899322d263dbcdb72b76591f5eb7ead4
RaboliotTheGreat <raboliotthegreat@mailoo.org>
2015-12-18 14:52:50 +0100

    Merge origin/master
    Conflicts:
    	src/Jeu/Monopoly.java

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -125,8 +125,6 @@ public class Monopoly {
                     
                     if(exaequo){
                     }
-                    
-                    }
                 }
                     
                     
